60ffb395877131a431559930a892d2746a3ed1b3
Dmitry <dmitry.balduev@gmail.com>
2021-05-24 12:03:04 -0400

    added proejcts routes

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { User, Project } = require('../../models');
+
+router.get('/', withAuth, async (req, res) => {
+    try{
+        const projectData = await Project.findAll();
+        const projects = projectData.map(each => each.get({ plain: true }));
+
+        res.status(200).json(projects);
+        
+    }catch(e){
+        res.status(500).json(err);
+
+    }
+    
+    
+  });
+
+
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+      const projectData = await Project.findOne({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!projectData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+
+      //10100000-10100000-10100000-10100000-10100000-10100000   ?      10100000-1010000010100000-10100000-10100000-10100000-10100000-10100000
+
+      let arrDrums = JSON.parse(projectData.content.split("?")[0]);
+      let arrMel = JSON.parse(projectData.content.split("?")[1]);
+
+      res.status(200).json(projectData);
+      res.render('project', { 
+        arrDrums, 
+        arrMel,
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
