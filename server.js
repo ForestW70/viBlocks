767ab39b5ad9da42c327e321987b28fe67098396
Dmitry <dmitry.balduev@gmail.com>
2021-06-01 13:16:00 -0400

    resolve confl

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({});
-<<<<<<< HEAD
-console.log('hi')
-=======
+console.log('hi');
 
->>>>>>> forest2
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
